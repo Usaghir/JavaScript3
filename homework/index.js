c1892411d224cc28fde4ace046e5070cd9ba920f
usaghir <umer.saghir@live.com>
2019-05-07 15:19:23 +0200

    Week3 HomeWork Compelted

--- homework/index.js
+++ homework/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 {
-  function fetchJSON(url) {
+  function fetchJSON(URL) {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
+      xhr.open('GET', URL);
       xhr.responseType = 'json';
       xhr.onload = () => {
         if (xhr.status < 400) {
@@ -57,105 +57,109 @@
     const updatedAt = new Date(element.updated_at).toLocaleString();
     createAndAppend('li', leftDetailID, { text: `Updated:  ${updatedAt}` });
   }
-  function loadContributor(element) {
-    fetchJSON(element.contributors_url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        const rightID = document.getElementById('rightID');
-        createAndAppend('p', rightID, { id: 'contTextID', text: 'Contributors' });
-        createAndAppend('ul', rightID, { id: 'contListID', class: 'contListClass' });
+  async function loadContributor(element) {
+    const root = document.getElementById('root');
+    try {
+      const data = await fetchJSON(element.contributors_url);
+      const rightID = document.getElementById('rightID');
+      createAndAppend('p', rightID, { id: 'contTextID', text: 'Contributors' });
+      createAndAppend('ul', rightID, { id: 'contListID', class: 'contListClass' });
 
-        let contURL;
-        let contData;
-        let contDetail;
-        const contListID = document.getElementById('contListID');
-        for (let i = 0; i < data.length; i++) {
-          contURL = createAndAppend('a', contListID, {
-            href: data[i].html_url,
-            target: '_blank',
-            id: 'contUrlID',
-            class: 'contUrlClass',
-          });
-          contData = createAndAppend('li', contURL, {
-            id: 'contDataID',
-            class: 'contDataClass',
-          });
-
-          createAndAppend('img', contData, {
-            src: data[i].avatar_url,
-            id: 'contImgID',
-            class: 'contImgClass',
-          });
-
-          contDetail = createAndAppend('div', contData, {
-            id: 'contDetailID',
-            class: 'contDetailClass',
-          });
-          createAndAppend('div', contDetail, { text: data[i].login });
-          createAndAppend('div', contDetail, {
-            text: data[i].contributions,
-            id: 'contBadgeID',
-            class: 'contBadgeClass',
-          });
-        }
-      }
-    });
-  }
-
-  function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        createAndAppend('header', root, { id: 'topBoxID', class: 'topBoxClass' });
-        const topBoxID = document.getElementById('topBoxID');
-        createAndAppend('p', topBoxID, {
-          id: 'topTextID',
-          class: 'topTextClass',
-          text: 'HYF Repositories',
+      let contURL;
+      let contData;
+      let contDetail;
+      const contListID = document.getElementById('contListID');
+      for (let i = 0; i < data.length; i++) {
+        contURL = createAndAppend('a', contListID, {
+          href: data[i].html_url,
+          target: '_blank',
+          id: 'contUrlID',
+          class: 'contUrlClass',
+        });
+        contData = createAndAppend('li', contURL, {
+          id: 'contDataID',
+          class: 'contDataClass',
         });
 
-        createAndAppend('div', root, { id: 'mainContainerID', class: 'mainContainerClass' });
+        createAndAppend('img', contData, {
+          src: data[i].avatar_url,
+          id: 'contImgID',
+          class: 'contImgClass',
+        });
 
-        const mainContainerID = document.getElementById('mainContainerID');
+        contDetail = createAndAppend('div', contData, {
+          id: 'contDetailID',
+          class: 'contDetailClass',
+        });
+        createAndAppend('div', contDetail, { text: data[i].login });
+        createAndAppend('div', contDetail, {
+          text: data[i].contributions,
+          id: 'contBadgeID',
+          class: 'contBadgeClass',
+        });
+      }
+    } catch (err) {
+      createAndAppend('div', root, {
+        id: 'err.message',
+        class: 'alert-error',
+      });
+    }
+  }
+
+  async function main(url) {
+    const root = document.getElementById('root');
+    try {
+      const data = await fetchJSON(url);
+      createAndAppend('header', root, {
+        id: 'topBoxID',
+        class: 'topBoxClass',
+      });
+      const topBoxID = document.getElementById('topBoxID');
+      createAndAppend('p', topBoxID, {
+        id: 'topTextID',
+        class: 'topTextClass',
+        text: 'HYF Repositories',
+      });
+
+      createAndAppend('div', root, { id: 'mainContainerID', class: 'mainContainerClass' });
+
+      const mainContainerID = document.getElementById('mainContainerID');
+      createAndAppend('div', mainContainerID, { id: 'leftID', class: 'leftClass' });
+      createAndAppend('div', mainContainerID, { id: 'rightID', class: 'rightClass' });
+      createAndAppend('select', topBoxID, { id: 'repsSelectID', Class: 'repsSelectClass' });
+      data.sort((a, b) => a.name.localeCompare(b.name));
+
+      const repsSelectID = document.getElementById('repsSelectID');
+      for (let index = 0; index < data.length; index++) {
+        createAndAppend('option', repsSelectID, {
+          value: index,
+          text: data[index].name,
+          id: 'optionID',
+          class: 'optionClass',
+        });
+      }
+      const selector = document.getElementById('repsSelectID');
+
+      loadDetail(data[0]);
+      loadContributor(data[0]);
+
+      selector.onchange = () => {
+        const leftID = document.getElementById('leftID');
+        leftID.parentNode.removeChild(leftID);
+
+        const rightID = document.getElementById('rightID');
+        rightID.parentNode.removeChild(rightID);
+
+        const i = selector.value;
+
         createAndAppend('div', mainContainerID, { id: 'leftID', class: 'leftClass' });
         createAndAppend('div', mainContainerID, { id: 'rightID', class: 'rightClass' });
-        createAndAppend('select', topBoxID, { id: 'repsSelectID', Class: 'repsSelectClass' });
-        data.sort((a, b) => a.name.localeCompare(b.name));
-
-        const repsSelectID = document.getElementById('repsSelectID');
-        for (let index = 0; index < data.length; index++) {
-          createAndAppend('option', repsSelectID, {
-            value: index,
-            text: data[index].name,
-            id: 'optionID',
-            class: 'optionClass',
-          });
-        }
-        const selector = document.getElementById('repsSelectID');
-
-        loadDetail(data[0]);
-        loadContributor(data[0]);
-
-        selector.onchange = () => {
-          const leftDetail = document.getElementById('leftID');
-          leftDetail.parentNode.removeChild(leftDetail);
-
-          const rightDetail = document.getElementById('rightID');
-          rightDetail.parentNode.removeChild(rightDetail);
-
-          const i = selector.value;
-
-          createAndAppend('div', mainContainerID, { id: 'leftID', class: 'leftClass' });
-          createAndAppend('div', mainContainerID, { id: 'rightID', class: 'rightClass' });
-          loadDetail(data[i]);
-          loadContributor(data[i]);
-        };
-      }
-    });
+        loadDetail(data[i]);
+        loadContributor(data[i]);
+      };
+    } catch (error) {
+      createAndAppend('div', root, { text: error.message, class: 'alert-error' });
+    }
   }
 
   const HYF_REPS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
